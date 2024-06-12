ffe7dded9fb2fea8dc5e0850b3d7277ed2a03b33
Myeongsu Yu <msu122005@hs.ac.kr>
2024-06-12 16:38:41 +0900

    feat: initial commit

---
+++ src/main/java/com/ductchpay/howmuch/HowmuchApplication.java
@@ -0,0 +1,13 @@
+package com.ductchpay.howmuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowmuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowmuchApplication.class, args);
+	}
+
+}
