1b5e7a635183da253df1bfc21cd5d050776dd833
Myeongsu Yu <msu122005@hs.ac.kr>
2024-06-13 00:22:42 +0900

    feat: 사용자 인증 로직 추가

---
+++ src/main/java/com/ductchpay/howmuch/config/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.ductchpay.howmuch.config;
+
+import com.ductchpay.howmuch.domain.Member;
+import com.ductchpay.howmuch.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+    }
+
+    private UserDetails createUserDetails(Member member) {
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getRole().toString());
+
+        return new User(member.getEmail(), member.getPassword(), Collections.singleton(grantedAuthority));
+    }
+}
