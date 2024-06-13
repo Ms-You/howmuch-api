4cc4f5abc780eab2e92c9cd66abe0a8244234f74
Myeongsu Yu <msu122005@hs.ac.kr>
2024-06-13 16:36:44 +0900

    feat: JWT 토큰 생성 및 검증 로직 추가

---
+++ src/main/java/com/ductchpay/howmuch/config/jwt/TokenDTO.java
@@ -0,0 +1,11 @@
+package com.ductchpay.howmuch.config.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenDTO {
+    private String accessToken;
+    private String refreshToken;
+}
