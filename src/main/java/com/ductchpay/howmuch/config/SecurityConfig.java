232cad91f868937ac22fa7d9878db5d132a676ce
Myeongsu Yu <msu122005@hs.ac.kr>
2024-06-13 17:32:32 +0900

    feat: Jwt 토큰을 이용한 사용자 인증 및 인가 처리

--- src/main/java/com/ductchpay/howmuch/config/SecurityConfig.java
+++ src/main/java/com/ductchpay/howmuch/config/SecurityConfig.java
@@ -1,19 +1,29 @@
 package com.ductchpay.howmuch.config;
 
+import com.ductchpay.howmuch.config.jwt.JwtAccessDeniedHandler;
+import com.ductchpay.howmuch.config.jwt.JwtAuthenticationEntryPoint;
+import com.ductchpay.howmuch.config.jwt.JwtAuthenticationFilter;
+import com.ductchpay.howmuch.config.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
     private final CorsConfigurationSource corsConfigurationSource;
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -32,7 +42,11 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.PUT, "/member/**").authenticated()
                         .requestMatchers(HttpMethod.PATCH, "/member/**").authenticated()
                         .requestMatchers(HttpMethod.DELETE, "/member/**").authenticated()
-                        .anyRequest().permitAll());
+                        .anyRequest().permitAll())
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler))
+                .addFilterBefore(new JwtAuthenticationFilter(tokenProvider, redisTemplate), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
---
+++ src/main/java/com/ductchpay/howmuch/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,57 @@
+package com.ductchpay.howmuch.config.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilterBean {
+    private final TokenProvider tokenProvider;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // AccessToken 추출
+        String token = resolveToken((HttpServletRequest) request);
+
+        // 토큰이 유요한지 확인
+        if(token != null && tokenProvider.validateToken(token)) {
+            // Redis 에서 토큰의 logout 여부 확인
+            String isLogout = redisTemplate.opsForValue().get(token);
+
+            // 로그아웃된 토큰이 아니라면 정상 처리
+            if(ObjectUtils.isEmpty(isLogout)) {
+                Authentication authentication = tokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * Request Header 에서 AccessToken 토큰 추출
+     * @param request
+     * @return
+     */
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+
+        return null;
+    }
+}
